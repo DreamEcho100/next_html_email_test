b2e277ea8f8ac6ec33a87a89b04f244c400befc1
DreamEcho100 <maze6572198@gmail.com>
2022-06-24 21:40:52 +0200

    Test

--- scripts/clean_out_from_scripts_tags.tsx
+++ scripts/clean_out_from_scripts_tags.tsx
@@ -1,9 +1,13 @@
+// https://nodejs.dev/learn/writing-files-with-nodejs
+// https://regexr.com/
+
 import * as fs from 'fs';
 import * as path from 'path';
 
-const cleanOutFromScriptsTags = async () => {
+export const cleanOutFromScriptsTags = async () => {
 	try {
 		const targetPath = path.join(__dirname, '../out/index.html');
+		console.log('targetPath', targetPath);
 		await fs.readFile(targetPath, { encoding: 'utf8' }, (err, data) => {
 			if (err) throw err.message;
 

8df0c2b97ed6001f3dcd282a0de63aec6fba8c62
DreamEcho100 <maze6572198@gmail.com>
2022-06-28 23:36:43 +0200

    Updating exported build

---
+++ _next/static/LsBfeOVvRYm6RUIa0TZMq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/547-cd310fdf06a1d81c.js","static/chunks/pages/index-71f16e856bf4ba0a.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
